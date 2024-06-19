f1c6d8ea70dede2e01091e88190c342fd8df59ca
yuriccosta <yuricoutinhoc@gmail.com>
2024-06-19 16:13:14 -0300

    Adicionado a funcionalidade de cadastrar e obter os eventos

--- Src/Calendario/calendario.py
+++ Src/Calendario/calendario.py
@@ -7,6 +7,9 @@ class calendario_UESC:
         # Data atual, util para pegar o dia, mês e ano assim que inicializar o programa 
         # e quando precisar voltar para o dia atual
         self.__dataAtual: datetime = datetime.now()
+
+        # Data auxiliar para percorrer o calendário
+        self.__dataAux: datetime = datetime.now()
         
         # Atributos referentes aos dias, mês, ano e dia da semana, inicialmente são os valores da data atual
         self.__dia: int = self.__dataAtual.day
@@ -19,7 +22,70 @@ class calendario_UESC:
         self.__listaMes: list[list[tuple[int, int]]]= self.__cal.monthdays2calendar(self.__ano, self.__mes)
         self.__listaAno: list[list[tuple[int, int]]] = self.__cal.yeardays2calendar(self.__ano, 12)
 
+        # Lista de eventos cadastrados
+        self.__eventosAno: dict[int, dict[int, dict[int, list[(datetime, str)]]]] = {}
 
+    '''
+        self.exemplo_do_evento = {
+            2021: {
+                6: {
+                    1: [(datetime(2021, 6, 1, 12, 0), "Reunião"), (datetime(2021, 6, 1, 15, 0), "Aula")],
+                    2: [(datetime(2021, 7, 1, 12, 0), "Reunião"), (datetime(2021, 7, 1, 15, 0), "Aula")],
+                },
+                7: {
+                    2: [(datetime(2021, 7, 1, 12, 0), "Reunião"), (datetime(2021, 7, 1, 15, 0), "Aula")],
+                }
+            }, 2022: {
+                1: {
+                    3: [(datetime(2022, 1, 1, 12, 0), "basquete"), (datetime(2022, 1, 1, 15, 0), "futebol")],
+                }, 31: {
+
+                    1: [(datetime(2022, 1, 31, 12, 0), "Curso"), (datetime(2022, 1, 31, 15, 0), "Projeto")],
+                }
+            }
+        }
+    '''
+
+    # Método que retorna os eventos do ano, caso não tenha eventos, retorna um dicionário vazio
+    def listaEventosAno(self, ano: int) -> dict[int, dict[int, list[tuple[datetime, str]]]]:
+        return self.__eventosAno.get(ano, {})
+
+    # Método que retorna os eventos do mês, caso não tenha eventos, retorna um dicionário vazio
+    def listaEventosMes(self, mes: int, ano: int) -> dict[int, list[tuple[datetime, str]]]: 
+        return self.listaEventosAno(ano).get(mes, {})
+
+    # Método que retorna os eventos do dia, caso não tenha eventos, retorna uma lista None
+    def listaEventosDia(self, dia: int, mes: int, ano: int) -> list[tuple[datetime, str]]:
+        return self.listaEventosMes(mes, ano).get(dia, None)
+
+
+    # Método auxiliar para adicionar um evento no atributo __eventosAno
+    def __adicionaEvento(self, dia: int, mes: int, ano: int, evento: tuple[datetime, str]) -> None:
+        # Verifica se o ano, mês e dia já estão no dicionário, caso não estejam, adiciona
+        if ano not in self.__eventosAno:
+            self.__eventosAno[ano] = {}
+        if mes not in self.__eventosAno[ano]:
+            self.__eventosAno[ano][mes] = {}
+        if dia not in self.__eventosAno[ano][mes]:
+            self.__eventosAno[ano][mes][dia] = []
+
+        self.__eventosAno[ano][mes][dia].append(evento)
+        
+
+    # Método para cadastrar um evento, retorna True se foi possível cadastrar o evento e False caso contrário
+    def cadastraEvento(self, dia: int, mes: int, ano: int, descricao: str = '', hora=0, minuto=0) -> bool:
+        self.__dataAtual = datetime.now()
+        evento = datetime(ano, mes, dia, hora, minuto)
+        evento = (evento, descricao)
+
+        # Verifica se a data do evento é maior que a data atual
+        if self.__dataAtual < evento[0]:
+            self.__adicionaEvento(dia, mes, ano, evento)
+            return True
+        
+        return False
+                
+    
     # Método para voltar o mês, a partir de self.__mes
     def voltaMes(self) -> None:
         self.__mes -= 1
@@ -27,6 +93,8 @@ class calendario_UESC:
         if self.__mes < 1:
             self.__mes = 12
             self.__ano -= 1
+        self.__dataAux = datetime(self.__ano, self.__mes, self.__dia)
+        self.__diaSemana = self.__dataAux.weekday()
         self.__listaMes = self.__cal.monthdays2calendar(self.__ano, self.__mes)
         self.__listaAno = self.__cal.yeardays2calendar(self.__ano, 12)
         
@@ -38,6 +106,8 @@ class calendario_UESC:
         if self.__mes > 12:
             self.__mes = 1
             self.__ano += 1
+        self.__dataAux = datetime(self.__ano, self.__mes, self.__dia)
+        self.__diaSemana = self.__dataAux.weekday()
         self.__listaMes = self.__cal.monthdays2calendar(self.__ano, self.__mes)
         self.__listaAno = self.__cal.yeardays2calendar(self.__ano, 12)
 
@@ -47,6 +117,10 @@ class calendario_UESC:
         self.__ano -= 1
         self.__dia = 1
         self.__mes = 1
+        self.__dataAux = datetime(self.__ano, self.__mes, self.__dia)
+        self.__diaSemana = self.__dataAux.weekday()
+        self.__listaMes = self.__cal.monthdays2calendar(self.__ano, self.__mes)
+        self.__listaAno = self.__cal.yeardays2calendar(self.__ano, 12)
 
 
     # Método para avançar o ano, a partir de self.__ano
@@ -54,6 +128,10 @@ class calendario_UESC:
         self.__ano += 1
         self.__dia = 1
         self.__mes = 1
+        self.__dataAux = datetime(self.__ano, self.__mes, self.__dia)
+        self.__diaSemana = self.__dataAux.weekday()
+        self.__listaMes = self.__cal.monthdays2calendar(self.__ano, self.__mes)
+        self.__listaAno = self.__cal.yeardays2calendar(self.__ano, 12)
 
 
     ''' # Método utilizando o relativedelta para voltar o mês
@@ -86,3 +164,6 @@ class calendario_UESC:
     
     def getListaAno(self):
         return self.__listaAno
+    
+    def getEventos(self):
+        return self.__eventosAno
--- Src/Calendario/exemplo.py
+++ Src/Calendario/exemplo.py
@@ -1,18 +1,33 @@
 from calendario import calendario_UESC
-
+from datetime import datetime
 cal = calendario_UESC()
 
 # cal começa com a data atual
 print(f'''
     Dia: {cal.getDia()}
     Mês: {cal.getMes()}
-    Ano: {cal.getAno()}''')
-
+    Ano: {cal.getAno()}
+    Eventos: {cal.getEventos()}''')
+'''
+# cal volta 10 meses
 for c in range(10):
     cal.voltaMes()
 
-# cal volta 10 meses
+'''
+print("Tentativas de cadastro de eventos")
+print(cal.cadastraEvento(20, 6, 2024, 'Dia do Cinema Brasileiro'))
+print(cal.cadastraEvento(1, 1, 2024, 'Ano Novo'))
+print(cal.cadastraEvento(25, 12, 2024, 'Natal'))
+print(cal.cadastraEvento(20, 6 , 2024, 'Prova', 22, 48))
+
 print(f'''
     Dia: {cal.getDia()}
     Mês: {cal.getMes()}
-    Ano: {cal.getAno()}''')
+    Ano: {cal.getAno()}
+    Eventos: {cal.getEventos()}''')
+
+
+data1 = datetime(2024, 6, 20, 23, 00 )
+data2 = datetime(2024, 6, 20, 23, 1)
+
+print(data1 < data2)
